1e51ba2f6249e398eef6f67b5b804d92a6f8ab6e
Irina Mitskevich <irina_petrova759@mail.ru>
2024-04-13 14:03:55 +0200

    Created project

---
+++ src/main/java/com/springboot/partycouplecounter/PartyCoupleCounterApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.partycouplecounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyCoupleCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyCoupleCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/partycouplecounter/PartyCoupleCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.partycouplecounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyCoupleCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
